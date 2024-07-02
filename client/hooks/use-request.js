81c4874756c2bf28aeb3b83ce19980cb1a743ad2
YuYuan-Chang <lawani321@gmail.com>
2024-07-01 21:35:55 -0400

    finish signin signout

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,31 +1,36 @@
-import axios from "axios";
-import { useState } from "react";
+import axios from 'axios';
+import { useState } from 'react';
 
-export default ({url, method, body, onSuccess}) => {
-    const [errors, setErrors] = useState(null);
+export default ({ url, method, body, onSuccess }) => {
+  const [errors, setErrors] = useState(null);
 
-    const doRequest = async () => {
-        try {
-            setErrors(null);
-            const response = await axios[method](url, body);
-            if (onSuccess) {
-                onSuccess(response.data);
-            }
-            return response.data;
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
 
-        } catch (err) {
-            setErrors(
-                <div className = "alert alert-danger">
-                    <h4>Ooops....</h4>
-                    <ul className="my-0">
-                    {err.response.data.errors.map(err => (
-                        <li key={err.message}>{err.message}</li>
-                    ))}
-                    </ul>
-                </div>
-            );
-        }
-    };
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
 
-    return {doRequest, errors};
-}
+      return response.data;
+    } catch (err) {
+      setErrors(
+        <div className="alert alert-danger">
+          <h4>Ooops....</h4>
+          <ul className="my-0">
+            {err.response && err.response.data && err.response.data.errors ? (
+              err.response.data.errors.map(err => (
+                <li key={err.message}>{err.message}</li>
+              ))
+            ) : (
+              <li>Something went wrong</li>
+            )}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return { doRequest, errors };
+};
