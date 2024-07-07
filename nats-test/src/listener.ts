c761ac0a836f356e80fab8cb147d870659d2e09f
YuYuan-Chang <lawani321@gmail.com>
2024-07-07 14:59:34 -0400

    update Nats

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, {Message} from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -15,29 +16,12 @@ stan.on('connect', () => {
         process.exit();
     });
 
-    const options = stan
-        .subscriptionOptions()
-        .setManualAckMode(true)
-        .setDeliverAllAvailable()
-        .setDurableName('accounting-service');
-
-    const subscription = stan.subscribe(
-        'ticket:created', 
-        'queue-group-name',
-        options
-    );
-
-    subscription.on('message', (msg: Message) => {
-        const data = msg.getData();
-
-        if (typeof data == 'string') {
-            console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-        }
-        msg.ack();
-    });
+    new TicketCreatedListener(stan).listen();
 });
 
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
+
+
 
