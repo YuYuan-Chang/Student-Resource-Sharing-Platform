eb8c584ad9e4884c4fdd6dead2ff37be214f565a
YuYuan-Chang <lawani321@gmail.com>
2024-07-09 15:44:08 -0400

    Update orders status

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,54 +1,58 @@
 import mongoose from 'mongoose';
 import { OrderStatus } from '@lawani321/common';
 import { TicketDoc } from './ticket';
+export { OrderStatus };
 
 interface OrderAttrs {
-    userId: string;
-    status: OrderStatus;
-    expiresAt: DataTransfer;
-    ticket: TicketDoc;
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
 }
 
 interface OrderDoc extends mongoose.Document {
-    userId: string;
-    status: OrderStatus;
-    expiresAt: DataTransfer;
-    ticket: TicketDoc;
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
 }
 
 interface OrderModel extends mongoose.Model<OrderDoc> {
-    build(attrs: OrderAttrs): OrderDoc;
+  build(attrs: OrderAttrs): OrderDoc;
 }
 
-const orderSchema = new mongoose.Schema({
+const orderSchema = new mongoose.Schema(
+  {
     userId: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     status: {
-        type: String,
-        required: true,
-        enum: Object.values(OrderStatus),
-        default: OrderStatus.Created,
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
     },
     expiresAt: {
-        type: mongoose.Schema.Types.Date
+      type: mongoose.Schema.Types.Date,
     },
     ticket: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Ticket'
-    }
-}, {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
     toJSON: {
-        transform(doc, ret) {
-            ret.id = ret._id;
-            delete ret._id;
-        }
-    }
-}); 
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
-    return new Order(attrs);
+  return new Order(attrs);
 };
 
 const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,5 +1,5 @@
-
 import mongoose from "mongoose";
+import { Order, OrderStatus } from "./order";
 
 interface TicketAttrs {
     title: string;
@@ -9,6 +9,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
+    isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -36,8 +37,24 @@ const ticketSchema = new mongoose.Schema({
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs);
+};
+ticketSchema.methods.isReserved = async function() {
+    // this === the ticket doc that we just call isReserved
+    const existingOrder = await Order.findOne({
+        ticket: this,
+        status: {
+            $in: [
+                OrderStatus.Created,
+                OrderStatus.AwaitingPayment,
+                OrderStatus.Complete
+            ]
+        }
+    });
+
+    return !!existingOrder;
 }
+
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
-export {Ticket}
+export {Ticket};
