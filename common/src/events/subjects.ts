a6dbddcf5467d5e7f90ccbed77df2c71e3928851
YuYuan-Chang <lawani321@gmail.com>
2024-07-10 16:14:42 -0400

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -1,6 +1,9 @@
 export enum Subjects {
     TicketCreated = 'ticket:created',
     TicketUpdate = 'ticket:updated',
+
+    OrderCreated = 'order:created',
+    OrderCancelled = 'order:cancelled',
 }
 
 
