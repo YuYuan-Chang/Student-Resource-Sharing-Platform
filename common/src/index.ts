5487498fa7224b0397ed9bed513f83f0f357a5e4
YuYuan-Chang <lawani321@gmail.com>
2024-07-10 18:01:38 -0400

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -10,8 +10,8 @@ export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
 
-export * from './events/base-publisher';
 export * from './events/base-listener';
+export * from './events/base-publisher';
 export * from './events/subjects';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
