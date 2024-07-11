1c6681e28dd12fbaa50fcdebf401f0cbd5972c4f
YuYuan-Chang <lawani321@gmail.com>
2024-07-11 11:15:10 -0400

    update ticket event test case

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,34 @@
+import { Ticket } from "../ticket";
+
+it('implements optimistic concurrency control', async () => {
+    //create an instance of ticket
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 5,
+        userId: '123'
+    });
+
+    //save the ticket to the database
+    await ticket.save();
+
+    //fetch the ticket twice
+    const firstInstance = await Ticket.findById(ticket.id);
+    const secondInstance = await Ticket.findById(ticket.id);
+
+    //make two separate changes to the tickets we fetched
+    firstInstance!.set({ price: 10});
+    secondInstance!.set({price: 15});
+
+    //save the first fetched ticket
+    await firstInstance!.save();
+
+
+    //save the second fetched ticket and expect an error
+    try {
+        await secondInstance!.save();
+    } catch (err) {
+        return;
+    }
+
+
+});
--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+
 
 interface TicketAttrs {
   title: string;
@@ -10,7 +13,9 @@ interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
   userId: string;
+  version: number;
 }
+
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
@@ -40,6 +45,9 @@ const ticketSchema = new mongoose.Schema(
     },
   }
 );
+
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
